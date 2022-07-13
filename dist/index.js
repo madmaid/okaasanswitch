9f0704da39df0b6341469d8cfd9fd2ab40306ea2
madmaid <madmaid@users.noreply.github.com>
2022-07-14 03:05:38 +0900

    build

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -19,13 +23,13 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var onoff_1 = require("onoff");
-var discord = __importStar(require("discord.js"));
-var fs = __importStar(require("fs"));
-var os = __importStar(require("os"));
-var path = __importStar(require("path"));
-var ledPin = 112;
-var led = new onoff_1.Gpio(ledPin, "out");
+const onoff_1 = require("onoff");
+const discord = __importStar(require("discord.js"));
+const fs = __importStar(require("fs"));
+const os = __importStar(require("os"));
+const path = __importStar(require("path"));
+const ledPin = 112;
+const led = new onoff_1.Gpio(ledPin, "out");
 function onExit() {
     led.writeSync(0);
     led.unexport();
@@ -33,34 +37,34 @@ function onExit() {
 }
 function lightLED(status) {
     led.read()
-        .then(function () { return led.write(status ? 1 : 0); })
-        .catch(function (err) { return console.log(err); });
+        .then(() => led.write(status ? 1 : 0))
+        .catch((err) => console.log(err));
 }
 function registerDiscordCallbacks(client, config) {
-    client.on('ready', function () {
+    client.on('ready', () => {
         var _a;
-        var tag = (_a = client === null || client === void 0 ? void 0 : client.user) === null || _a === void 0 ? void 0 : _a.tag;
-        console.log("Logged in as " + tag + "!");
+        const tag = (_a = client === null || client === void 0 ? void 0 : client.user) === null || _a === void 0 ? void 0 : _a.tag;
+        console.log(`Logged in as ${tag}!`);
     });
-    client.on("presenceUpdate", function (_, triggered) {
+    client.on("presenceUpdate", (_, triggered) => {
         var _a, _b, _c;
         if (((_a = triggered.user) === null || _a === void 0 ? void 0 : _a.username) !== config.username) {
             return;
         }
         ;
-        var userStatus = (_c = (_b = triggered.user) === null || _b === void 0 ? void 0 : _b.presence) === null || _c === void 0 ? void 0 : _c.status;
+        const userStatus = (_c = (_b = triggered.member) === null || _b === void 0 ? void 0 : _b.presence) === null || _c === void 0 ? void 0 : _c.status;
         lightLED(userStatus === "online");
     });
 }
 (function main() {
-    var systemConfigPath = "/usr/local/etc/okaasanswitch/config.json";
-    var homePath = os.homedir();
-    var configPath = homePath === "/root"
+    const systemConfigPath = "/usr/local/etc/okaasanswitch/config.json";
+    const homePath = os.homedir();
+    const configPath = homePath === "/root"
         ? systemConfigPath
         : path.join(homePath, ".config/okaasanswitch/config.json");
     console.log("load config :", configPath);
-    var config = JSON.parse(fs.readFileSync(configPath, "utf8"));
-    var client = new discord.Client();
+    const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+    const client = new discord.Client({ intents: ['GUILDS', "GUILD_MEMBERS", "GUILD_PRESENCES"] });
     registerDiscordCallbacks(client, config.discord);
     client.login(config.discord.secret);
     process.on("SIGINT", onExit);
